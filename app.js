9e7301e3b76fc4a259d1981a049f63e923459308
cn-gourav <mrgusain558@gmail.com>
2025-05-06 21:53:20 +0530

    Create :: EJs file and install

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine" , 'ejs')
+
 app.get('/', (req,res) =>{
-    res.send('hello world')
+    res.render('index')
 })
 
 app.get('about', (req ,res) =>{
