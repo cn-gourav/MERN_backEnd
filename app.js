b1c9c76d14afb530acb7396c6709b2d8eb84fd19
cn-gourav <mrgusain558@gmail.com>
2025-05-06 23:37:39 +0530

    Put data in db by using mangoose

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan')
-const userModel = require('./models/user')
-const dbconnection = require('./config/db')
+const morgan = require("morgan");
+const userModel = require("./models/user");
+const dbconnection = require("./config/db");
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
-app.set('view engine','ejs')
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/',(req,res) =>{
-    res.render('index')
-})
+app.get("about", (req, res) => {
+  res.send("about page");
+});
 
-app.get('about', (req ,res) =>{
-    res.send('about page')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.post('/get-form-data' , (req,res) =>{
-    console.log(req.body)
-    res.send('data received')
-})
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
-app.listen(3000 , ()=>{
-    console.log('server listen')
-})
+const newUser =  await userModel.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+  res.send(newUser);
+});
 
+app.post("/get-form-data", (req, res) => {
+  console.log(req.body);
+  res.send("data received");
+});
 
+app.listen(3000, () => {
+  console.log("server listen");
+});
 
 // const http = require('http');
 // const { url } = require('inspector');
