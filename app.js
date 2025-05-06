5630eaa11b403bd4abf9be0ef8d18cc59c0fb3c7
cn-gourav <mrgusain558@gmail.com>
2025-05-06 21:50:40 +0530

    Create :: server by using express

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const { url } = require('inspector');
 
-const server = http.createServer((req ,res) =>{
-    if(req.url == "/about"){
-        res.end("The about app")
-    }
-
-    if(req.url == "/"){
-        res.end('hello world')
-    }
-    console.log(req.url)
+app.get('/', (req,res) =>{
+    res.send('hello world')
 })
 
-server.listen(3000 , () =>{
-    console.log("Server listening....")
+app.get('about', (req ,res) =>{
+    res.send('about page')
 })
+
+app.listen(3000 , ()=>{
+    console.log('server listen')
+})
+
+
+
+// const http = require('http');
+// const { url } = require('inspector');
+
+// const server = http.createServer((req ,res) =>{
+//     if(req.url == "/about"){
+//         res.end("The about app")
+//     }
+
+//     if(req.url == "/"){
+//         res.end('hello world')
+//     }
+//     console.log(req.url)
+// })
+
+// server.listen(3000 , () =>{
+//     console.log("Server listening....")
+// })
