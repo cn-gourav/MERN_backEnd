a7d2f1b122cd61ca84aad4bf5c1393f54fe94dac
cn-gourav <mrgusain558@gmail.com>
2025-05-06 23:16:32 +0530

    install mangoose create connection and connected

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
-
+const userModel = require('./models/user')
+const dbconnection = require('./config/db')
 
 app.use(morgan('dev'))
 app.use(express.json())
