ffb173065028e6c524e45b868c7fc30eba36afdf
cn-gourav <mrgusain558@gmail.com>
2025-05-06 22:35:43 +0530

    handing user form detail username password email is url

--- app.js
+++ app.js
@@ -4,28 +4,22 @@ const morgan = require('morgan')
 
 
 app.use(morgan('dev'))
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 
-app.use((req,res,next)=>{
-    console.log('this is middleware')
-    // res.send("this is middleware")
-    morgan(':method :url :status :res[content-length] - :response-time ms')
-    return next()
-})
 
-app.get('/', (req,res,next) =>{
-    const a = 5
-    const b = 6
-
-    console.log(a+b)
-    return next()
-} ,(req,res) =>{
+app.get('/',(req,res) =>{
     res.render('index')
 })
 
 app.get('about', (req ,res) =>{
     res.send('about page')
+})
+
+app.post('/get-form-data' , (req,res) =>{
+    console.log(req.body)
+    res.send('data received')
 })
 
 app.listen(3000 , ()=>{
